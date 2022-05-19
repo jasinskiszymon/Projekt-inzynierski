2889b8ef377849c238f5689cfd828a1a9e2d400b
jasinskiszymon <62395052+jasinskiszymon@users.noreply.github.com>
2022-05-19 16:24:14 +0200

    simple change

--- CreateNow/my_app/src/App.tsx
+++ CreateNow/my_app/src/App.tsx
@@ -1,16 +1,20 @@
 import React, {FC} from 'react';
 import Topnav from './components/topnav/topnav';
-
+import Creator from './components/creator/creator';
+import Offers from './components/offers/offers';
 export const AppWrapper: FC = () => {
   return (
    
     <div>
        <Topnav />
+       
        <div>
       Lorem Ipsum is simply dummy text of the printing and typesetting industry.
       Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
       when an unknown printer took a galley of type and scrambled it to
       </div>
+      <Creator/>
+      <Offers/>
     </div>
   );
 };
---
+++ CreateNow/my_app/src/components/offers/offers.tsx
@@ -0,0 +1,14 @@
+import React, {FC} from 'react';
+import styled  from 'styled-components';
+const Wrapper = styled.div`
+    
+`;
+export const Offers: FC = () => {
+  return (
+      <Wrapper>
+          Hello world, i'm list of offers.
+      </Wrapper>
+  );
+
+  }
+  export default Offers
